2131eb2c6826f891be4946ef40bc68e754e08807
Aristide <dovi.aristide@gmail.com>
2024-07-10 00:35:22 +0200

    update invoice create vue

--- resources/views/invoices/create.blade.php
+++ resources/views/invoices/create.blade.php
@@ -12,60 +12,86 @@
         <div class="content-body">
 
         <div class="container">
-    <h1>Create Invoice</h1>
-    <form action="{{ route('invoices.store') }}" method="POST">
-        @csrf
-        <div class="form-group">
-            <label for="patient_id">patient</label>
-            <select name="patient_id" id="patient_id" class="form-control" required>
-                <option value="">Select patient</option>
-                @foreach($patients as $patient)
-                    <option value="{{ $patient->id }}">{{ $patient->code }} ({{ $patient->first_name }} {{ $patient->last_name }})</option>
-                @endforeach
-            </select>
+            <h1>Formulaire de création de Facture</h1>
+            <form action="{{ route('invoices.store') }}" method="POST">
+                @csrf
+                <div class="row">
+                    <div class="col-md-6">
+
+                    </div>
+                    <div class="col-md-6 text-right">
+                        <button type="submit" class="btn btn-success">Enregistrer Facture</button>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="form-row">
+                            <div class="form-group col-md-4">
+                                <label for="patient_id">Information du patient</label>
+                                <select name="patient_id" id="patient_id" class="form-control" required>
+                                    <option value="">Select patient</option>
+                                    @foreach($patients as $patient)
+                                        <option value="{{ $patient->id }}">{{ $patient->code }} ({{ $patient->first_name }} {{ $patient->last_name }})</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="form-group col-md-4">
+                                <label for="invoice_date">Date de Facturation</label>
+                                <input type="date" name="invoice_date" id="invoice_date" class="form-control" required>
+                            </div>
+                            <div class="form-group col-md-4">
+                                <label for="due_date">Date d'échéance</label>
+                                <input type="date" name="due_date" id="due_date" class="form-control" required>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="items">Les élément du devis</label>
+                            <table class="table" id="items-table">
+                                <!-- <thead>
+                                    <tr>
+                                        <th>Description</th>
+                                        <th>Quantity</th>
+                                        <th>Price</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead> -->
+                                <tbody>
+                                    <tr>
+                                        <!-- <td>
+                                            <button type="button" class="btn btn-danger remove-item btn-sm">
+                                                <i class="fa fa-close"></i>
+                                            </button>
+                                        </td> -->
+                                        <td class="pt-0 pl-0" style="width: 40%;" >
+                                            <select name="items[0][item_id]" class="form-control item-select" required>
+                                                <option value="">Select élément</option>
+                                                @foreach($availableItems as $item)
+                                                    <option value="{{ $item->id }}" data-price="{{ $item->price }}">{{ $item->name }}</option>
+                                                @endforeach
+                                            </select>   
+                                        <!-- <input type="text" name="items[0][description]" class="form-control" required> -->
+                                        </td>
+                                        <td class="pt-0" style="width: 30%;"><input type="number" name="items[0][quantity]" class="form-control" placeholder="Quantité" required></td>
+                                        <td class="pt-0" style="width: 30%;"><input type="number" name="items[0][price]" class="form-control" placeholder="Prix unitaire" required></td>
+                                        <td class="pt-0">
+                                            <button type="button" class="btn btn-primary btn-sm" id="add-item">
+                                            <i class="fa fa-plus"></i>
+                                            </button>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <!-- <button type="submit" class="btn btn-success">Create Invoice</button> -->
+            </form>
         </div>
-        <div class="form-group">
-            <label for="invoice_date">Invoice Date</label>
-            <input type="date" name="invoice_date" id="invoice_date" class="form-control" required>
-        </div>
-        <div class="form-group">
-            <label for="due_date">Due Date</label>
-            <input type="date" name="due_date" id="due_date" class="form-control" required>
-        </div>
-        <div class="form-group">
-            <label for="items">Items</label>
-            <table class="table" id="items-table">
-                <thead>
-                    <tr>
-                        <th>Description</th>
-                        <th>Quantity</th>
-                        <th>Price</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>
-                            <select name="items[0][item_id]" class="form-control item-select" required>
-                                <option value="">Select item</option>
-                                @foreach($availableItems as $item)
-                                    <option value="{{ $item->id }}" data-price="{{ $item->price }}">{{ $item->name }}</option>
-                                @endforeach
-                            </select>   
-                        <!-- <input type="text" name="items[0][description]" class="form-control" required> -->
-                        </td>
-                        <td><input type="number" name="items[0][quantity]" class="form-control" required></td>
-                        <td><input type="number" name="items[0][price]" class="form-control" required></td>
-                        <td><button type="button" class="btn btn-danger remove-item">Remove</button></td>
-                    </tr>
-                </tbody>
-            </table>
-            <button type="button" class="btn btn-primary" id="add-item">Add Item</button>
-        </div>
-        <button type="submit" class="btn btn-success">Create Invoice</button>
-    </form>
-</div>
-<!-- <input type="text" name="items[${itemIndex}][description]" class="form-control" required> -->
+<!-- <input type="text" name="items[${itemIndex}][description]" class="form-control" required> 
+                 <td><button type="button" class="btn btn-danger remove-item">Remove</button></td>
+-->
 <script>
     document.addEventListener('DOMContentLoaded', function () {
         let itemIndex = 1;
@@ -75,20 +101,26 @@
             const newRow = document.createElement('tr');
 
             newRow.innerHTML = `
-                <td>
+                <td class="pt-0 pl-0" style="width: 40%;">
                     <select name="items[${itemIndex}][item_id]" class="form-control item-select" required>
-                        <option value="">Select item</option>
+                        <option value="">Select élément</option>
                         @foreach($availableItems as $item)
                             <option value="{{ $item->id }}" data-price="{{ $item->price }}">{{ $item->name }}</option>
                         @endforeach
                     </select>
                 </td>
-                <td><input type="number" name="items[${itemIndex}][quantity]" class="form-control" required></td>
-                <td><input type="number" name="items[${itemIndex}][price]" class="form-control" required></td>
-                <td><button type="button" class="btn btn-danger remove-item">Remove</button></td>
+                <td class="pt-0" style="width: 30%;"><input type="number" name="items[${itemIndex}][quantity]" class="form-control" placeholder="Quantité" required></td>
+                <td class="pt-0" style="width: 30%;"><input type="number" name="items[${itemIndex}][price]" class="form-control" placeholder="Prix unitaire" required></td>
+                <td class="pt-0">
+                    <button type="button" class="btn btn-danger btn-sm remove-item">
+                        <i class="fa fa-close"></i>
+                    </button>
+                </td>
             `;
 
             tableBody.appendChild(newRow);
+            //tableBody.insertBefore(newRow, tableBody.firstChild);
+
             itemIndex++;
         });
 
@@ -107,6 +139,18 @@
             }
         });
     });
+
+    // Obtenir la date d'aujourd'hui
+    var today = new Date();
+
+    // Formater la date en YYYY-MM-DD
+    var year = today.getFullYear();
+    var month = (today.getMonth() + 1).toString().padStart(2, '0');
+    var day = today.getDate().toString().padStart(2, '0');
+    var formattedDate = year + '-' + month + '-' + day;
+
+    // Définir la valeur du champ de date
+    document.getElementById('invoice_date').value = formattedDate;
 </script>
 
         </div>
